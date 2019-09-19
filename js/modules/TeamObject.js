57f53fa07cb5c6cbb5c8224d076c74d0dc93e579
lkeery <l_keery@fanshaweonline.ca>
2019-09-19 12:02:28 -0400

    Added Javascript for team, contact form, our work, and footer

---
+++ js/modules/TeamObject.js
@@ -0,0 +1,35 @@
+// Vivid Agency Team
+
+const Team = {
+
+    Liam: {
+        name: "Liam Keery",
+        role: "UX Designer & Developer",
+        bio: "PLACEHOLDER",
+        avatar: "images/liam.png"
+    },
+
+    Noah: {
+        name: "Noah Fainer",
+        role: "Motion Designer",
+        bio: "PLACEHOLDER",
+        avatar: "images/noah.png"
+    },
+
+    Natalie: {
+        name: "Natalie Soberano",
+        role: "Designer & Copywriter",
+        bio: "PLACEHOLDER",
+        avatar: "images/natalie.png"
+    },
+
+    Yasmina: {
+        name: "Yasmina Mayot",
+        role: "Designer & Illustrator",
+        bio: "PLACEHOLDER",
+        avatar: "images/yasmina.png"
+    }
+
+}
+
+export default Team;
