98063542c6630632776396566ced7b3f3183c7cc
Your Name <n_fainer@fanshaweonline.ca>
2019-09-20 12:56:09 -0400

    addd video player and video

--- js/main.js
+++ js/main.js
@@ -21,4 +21,45 @@ import Team from "./modules/TeamObject.js";
 
         nextMemberToggle.addEventListener('click', displayMember)
     }
+
+
+
+    // video player starts here 
+
+    let vidPlayer = document.querySelector('#trailer');
+    let playBtn = document.querySelector('.playPause');
+    let flScrn = document.querySelector('.flScreen');
+    
+
+    function pausePlay(){
+        if (vidPlayer.paused == true){
+          vidPlayer.play();
+      
+      
+        } else {
+          vidPlayer.pause();
+        }
+    }
+
+    function fullScreenVideo(){
+        if (vidPlayer.requestFullscreen) {
+            vidPlayer.requestFullscreen();
+          } else if (vidPlayer.mozRequestFullScreen) { /* Firefox */
+            vidPlayer.mozRequestFullScreen();
+          } else if (vidPlayer.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+            vidPlayer.webkitRequestFullscreen();
+          } else if (vidPlayer.msRequestFullscreen) { /* IE/Edge */
+            vidPlayer.msRequestFullscreen();
+          }
+    }
+
+    
+
+    playBtn.addEventListener('click', pausePlay);
+    flScrn.addEventListener('click', fullScreenVideo);
+
+
+ 
+
+
 })();
